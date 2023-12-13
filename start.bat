0715e4499689844b550440324cd5717acf449cd5
Evil Squirrel <evil.squirrelo.xtreme@gmail.com>
2023-12-13 01:33:04 +0000

    i forgot how comments work in batch

--- start.bat
+++ start.bat
@@ -1,10 +1,10 @@
-; install pnpm if not installed
+:: install pnpm if not installed
 FOR /F "tokens=*" %%g IN ('WHERE pnpm') do (SET PNPM_PATH=%%g)
 IF NOT EXIST %PNPM_PATH% (
   npm i -g pnpm
 )
 
-; install dependencies if not already installed
+:: install dependencies if not already installed
 IF NOT EXIST node_modules (
   pnpm i
 )
