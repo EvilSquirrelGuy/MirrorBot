0b0de5e7bdb76e1bc2d9923c3fefe8320c56bc6b
Evil Squirrel <evil.squirrelo.xtreme@gmail.com>
2023-12-13 01:31:11 +0000

    create start for windows

---
+++ start.bat
@@ -0,0 +1,12 @@
+; install pnpm if not installed
+FOR /F "tokens=*" %%g IN ('WHERE pnpm') do (SET PNPM_PATH=%%g)
+IF NOT EXIST %PNPM_PATH% (
+  npm i -g pnpm
+)
+
+; install dependencies if not already installed
+IF NOT EXIST node_modules (
+  pnpm i
+)
+
+pnpm run start
